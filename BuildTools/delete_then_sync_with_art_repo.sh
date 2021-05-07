6db14256e9f4be047acfed558930ffdd21bd8bff
Lijun-Dev <jun.li@dragonplus.com>
2021-05-07 19:05:30 +0800

    ssss

--- BuildTools/delete_then_sync_with_art_repo.sh
+++ BuildTools/delete_then_sync_with_art_repo.sh
@@ -3,5 +3,5 @@
 echo "开始将美术资源同步至代码仓库"
 echo "Syncing Dev01Res to Dev01"
 
-rsync -av --delete ../../Lijun_Dev01Res/Dev01Res/Assets/Export ../Dev01/Assets
+rsync -arv --delete ../../Lijun_Dev01Res/Dev01Res/Assets/Export/ ../Dev01/Assets/Export/
 

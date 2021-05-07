41522d7da946af3c9fd6c9eca915ff7f9f3737da
Lijun-Dev <jun.li@dragonplus.com>
2021-05-07 18:47:31 +0800

    mod

--- git_then_sync_res.sh
+++ git_then_sync_res.sh
@@ -1,4 +1,4 @@
-cd $(dirname $0)/../Dev01Res
+cd $(dirname $0)/../Lijun_Dev01Res
 git stash
 git pull --rebase
 
@@ -7,6 +7,6 @@ if [ $? -eq 0 ];then
 	./delete_then_sync_with_art_repo.sh
 fi
 
-cd $(dirname $0)/../Dev01Res
+cd $(dirname $0)/../Lijun_Dev01Res
 git stash pop
 cd $(dirname $0)
